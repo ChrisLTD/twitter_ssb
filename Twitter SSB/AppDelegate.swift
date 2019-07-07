fa6d8205e7e373ea07386eb380021aec634e6412
Chris Johnson <chrisltd@gmail.com>
2019-07-07 15:56:47 -0400

    Initial Commit

---
+++ Twitter SSB/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Twitter SSB
+//
+//  Created by Chris Johnson on 7/7/19.
+//  Copyright © 2019 Yoeyo, Ltd. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Twitter SSB/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Twitter SSB
+//
+//  Created by Chris Johnson on 7/7/19.
+//  Copyright © 2019 Yoeyo, Ltd. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
